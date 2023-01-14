d8bb0df7849678bd0a8562f8f64a2c627b1b8396
Md Rizwan Alam <mdrizwanalamarmy@gmail.com>
2023-01-14 19:49:31 +0530

    Rizwan's first commit

---
+++ PracOutput.js
@@ -0,0 +1,49 @@
+
+// // console.log("Hello Mr. Ajay")
+// // // document.write("hello jsr \n" )
+
+
+// let x=12;
+// let y=1;
+// let sum=x+y;
+// console.log(sum);
+
+// function addthreenum(a,b,c){
+//     return a+b+c;
+// }
+// let sum = addthreenum(1,2,3);
+// console.log(sum)
+
+
+// console.log ("rizwan is nice guy")
+
+//  let a= "Rizwan"
+//  let b = 786
+//   console.log(typeof(a+b))
+
+// const dict = {
+//     yeuk: "to itch",
+//     yakka: "hard work",
+//     democracy:"government by the people",
+//     petrichor: "scent produced by rainfall on very dry ground",
+//     baboonery: "attitude"
+// }
+
+// document.write(dict['baboonery'])
+
+// ------------operators and expression----
+
+// console.log("Rizwan loves Boxing")
+//  let a= 2;
+//  let b= 3;
+//  console.log("a + b = ", (a+b))
+//  console.log("a - b = ", (a-b))
+//  console.log("a * b = ", (a*b))
+//  console.log("a / b = ", (a/b))
+//  console.log("a % b = ", (a%b))
+
+// -----Exponential operator------
+
+ let a= 2;
+ let b=3;
+ console.log(" 2 to the power 3 = " , a**b)
